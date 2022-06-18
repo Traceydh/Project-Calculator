cf5ae0ea6cabb0df393fdd1ab719c5ae0ed0e05e
tracey <tracey.d.huang@gmail.com>
2022-06-18 13:38:52 +1000

    Create operate function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//ADD function 
+ //ADD function 
 const add = function(num1, num2) {
 	return num1+num2;
 };
@@ -14,10 +14,20 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
     return num1/num2;
   };
-  let num1 = 2;
-  let num2 = 4;
 
-  console.log(add(num1, num2));
-  console.log(subtract(num1, num2));
-  console.log(multiply(num1, num2));
-  console.log(divide(num1, num2));
+//OPERATE function 
+//takes an operator and 2 numbers and then calls one of the above functions on the numbers
+function operate(num1,operator,num2) {
+    switch (operator) {
+        case '*': 
+        return multiply(num1, num2);
+        case '+': 
+        return add(num1, num2);
+        case '-': 
+        return subtract(num1, num2);
+        case '/': 
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate(2,'*',4));
