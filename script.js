e747bef9e54b45e6895838a2c07ec34270a061a1
tracey <tracey.d.huang@gmail.com>
2022-06-18 13:23:48 +1000

    Add and test add, subtract, divide and multiply functions

---
+++ script.js
@@ -0,0 +1,23 @@
+//ADD function 
+const add = function(num1, num2) {
+	return num1+num2;
+};
+//SUBTRACT function 
+const subtract = function(num1, num2) {
+	return num1-num2;
+};
+//MULTIPLY function
+const multiply = function(num1, num2) {
+    return num1*num2;
+  };
+//DIVIDE function 
+const divide = function(num1, num2) {
+    return num1/num2;
+  };
+  let num1 = 2;
+  let num2 = 4;
+
+  console.log(add(num1, num2));
+  console.log(subtract(num1, num2));
+  console.log(multiply(num1, num2));
+  console.log(divide(num1, num2));
