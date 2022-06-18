51478b78fd2a9d5530b1cc95333e4cc4b1fe8512
tracey <tracey.d.huang@gmail.com>
2022-06-18 13:59:54 +1000

    Create buttons

--- script.js
+++ script.js
@@ -19,15 +19,14 @@ const divide = function(num1, num2) {
 //takes an operator and 2 numbers and then calls one of the above functions on the numbers
 function operate(num1,operator,num2) {
     switch (operator) {
-        case '*': 
+        case '×': 
         return multiply(num1, num2);
         case '+': 
         return add(num1, num2);
         case '-': 
         return subtract(num1, num2);
-        case '/': 
+        case '÷': 
         return divide(num1, num2);
     }
 }
 
-console.log(operate(2,'*',4));
