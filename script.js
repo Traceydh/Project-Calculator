ad50b6738834903dcd47f544624d94823fb5035a
tracey <tracey.d.huang@gmail.com>
2022-06-18 22:08:05 +1000

    Add clear button which gets rid of display value

--- script.js
+++ script.js
@@ -13,27 +13,23 @@ function display() {
     //User enters number, display number on screen 
     displayValue.textContent += this.value;
     let string = displayValue.textContent;
-    console.log('string: '+ string);
+
+    //if user presses AC, clear value
+    const clearButton = document.querySelector('#clear');
+    clearButton.onclick = () => {displayValue.textContent = ''};
+
     //User presses operator, display operator, store as num1, store operator 
-    const plusButton = document.querySelector('#plus');
-    let num1;
-    let operator = '+';
-    plusButton.onclick = () => {
-        return num1 = string.slice(0,-1);
-    }
-    console.log(operator);
     //User enters another number, display on screen
     //user presses operator, display solution of num1 num2, store this as new num1, store new operator 
     //OR user presses =, store num 2, execute operate function, display solution, store solution as num1 
     const equalButton = document.querySelector('#equal');
     equalButton.onclick = () => {
-        let num2Array = string.split('+');
-        let num2 = num2Array[1].slice(0,-1);
-        console.log('num2: ' + num2);
-        console.log('operator: ' + operator);
-        console.log('num1: ' + num1);
-        let solution = operate(num1,operator,4);
-        console.log(solution);
+        let operator = '+';
+        let numArray = string.split('+');
+        let num2 = numArray[1].slice(0,-1);
+        let num1 = numArray[0];
+        let solution = operate(num1,operator,num2);
+        displayValue.textContent = solution;
     }
 }
 
@@ -57,6 +53,8 @@ const divide = function(num1, num2) {
 //OPERATE function 
 //takes an operator and 2 numbers and then calls one of the above functions on the numbers
 function operate(num1,operator,num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
     switch (operator) {
         case '×': 
         return multiply(num1, num2);
