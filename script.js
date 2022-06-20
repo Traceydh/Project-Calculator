15eb9addfbd11b5d6c2b7bbab34e32e4e59496de
tracey <tracey.d.huang@gmail.com>
2022-06-20 11:24:43 +1000

    Reset display number to zero when AC is clicked

--- script.js
+++ script.js
@@ -3,58 +3,85 @@ const allButtons = document.querySelectorAll('button');
 //loop through each button and set it's value as the same as it's content
 allButtons.forEach(button => button.setAttribute('value', `${button.textContent}`));
 
+let initialNum = 0;
 //Get display value 
 const displayValue = document.querySelector('#display-value');
+displayValue.textContent = `${initialNum}`
 //Add an eventListener for each button 
 allButtons.forEach(button => button.addEventListener('click', display));
+//store num1 as global variable
 
-//Display number pressed and solution 
 function display() {
-    //User enters number, display number on screen 
+    let num1 = ' '; 
+    num1 = num1 + this.value;
+    console.log(num1);
+
+    //console.log(this.value);
     displayValue.textContent += this.value;
-    let string = displayValue.textContent;
 
-    //if user presses AC, clear value
-    const clearButton = document.querySelector('#clear');
-    clearButton.onclick = () => {displayValue.textContent = ''};
-
-    const equalButton = document.querySelector('#equal');
-    equalButton.onclick = () => {
-        //Identify which operator is selected 
-        //ISSUE only identifies first operator, ignores the rest 
-        let operator;
-        if (string.includes('+')) {
-            operator = '+';
-        } else if (string.includes('×')) {
-            operator = '×';
-        } else if (string.includes('−')) {
-            operator = '−';
-        } else if (string.includes('÷')) {
-            operator = '÷';
-        }
-
-        //split string 
-        //ISSUE can't handle more numbers
-        let numArray = string.split(operator);
-        //store num1 and 2 
-        let num2 = numArray[1].slice(0,-1);
-        console.log('num2: ' + num2);
-        let num1 = numArray[0];
-        console.log('num1: ' + num1);
-        //calculate solution 
-        let solution = operate(num1,operator,num2);
-        displayValue.textContent = solution;
+    //if AC is pressed, reset num to 0 
+    if (this.value == 'AC') {
+        initialNum = 0;
+        displayValue.textContent = `${initialNum}`;
     }
-    console.log('string is: '+ string)
 }
 
 //functions can't return global variables 
-//user presses a number, store value as num1 
+//user presses a number, store value as num1
+    //How does computer know to update 0 to number? 
+    //How does computer know to store multiple numbers together? 
+    //How does computer know when to stop storing value as num1? 
 //user presses an operator, stop storing value, store operator 
 //user presses another number, store value as num2 
 //user presses =, display solution, store solution as num1 
 
+//store number before operator as num1 
+const plusButton = document.querySelector('#plus');
+plusButton.onclick = () => {
+    num1 = 3;
+};
 
+// //Display number pressed and solution 
+// function display() {
+//     //User enters number, display number on screen 
+//     displayValue.textContent += this.value;
+//     let string = displayValue.textContent;
+
+//     //if user presses AC, clear value
+//     const clearButton = document.querySelector('#clear');
+//     clearButton.onclick = () => {displayValue.textContent = ''};
+
+//     const equalButton = document.querySelector('#equal');
+//     equalButton.onclick = () => {
+//         //Identify which operator is selected 
+
+
+//         //ISSUE only identifies first operator, ignores the rest 
+//         let operator;
+//         if (string.includes('+')) {
+//             operator = '+';
+//         } else if (string.includes('×')) {
+//             operator = '×';
+//         } else if (string.includes('−')) {
+//             operator = '−';
+//         } else if (string.includes('÷')) {
+//             operator = '÷';
+//         }
+
+//         //split string 
+//         //ISSUE can't handle more numbers
+//         let numArray = string.split(operator);
+//         //store num1 and 2 
+//         let num2 = numArray[1].slice(0,-1);
+//         console.log('num2: ' + num2);
+//         let num1 = numArray[0];
+//         console.log('num1: ' + num1);
+//         //calculate solution 
+//         let solution = operate(num1,operator,num2);
+//         displayValue.textContent = solution;
+//     }
+//     console.log('string is: '+ string)
+// }
 
 //ADD function 
 const add = function(num1, num2) {
