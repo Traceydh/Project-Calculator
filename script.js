9d706e5a2796a3e4f15e82294d905d8bf25aca48
tracey <tracey.d.huang@gmail.com>
2022-06-21 14:05:53 +1000

    Console log the button number when clicked

--- script.js
+++ script.js
@@ -13,21 +13,61 @@ const clearButton = document.querySelector('#clear');
 const currentDisplay = document.querySelector('#currentDisplay');
 const previousDisplay = document.querySelector('#previousDisplay');
 
+//Apply display number function for numbers 
+numberButton.forEach(button => button.addEventListener('click', (e) => {
+    console.log(e.target.textContent);
+} ));
 
 
-const allButtons = document.querySelectorAll('button');
-allButtons.forEach(button => button.setAttribute('value', `${button.textContent}`));
-allButtons.forEach(button => button.addEventListener('click', display));
 
-let initialNum = 0;
-let num1;
-let num2;
-let num;
-let string = '0';
-let operator = '';
-let solution = '';
-let test = '';
-test = string;
+
+// const allButtons = document.querySelectorAll('button');
+// allButtons.forEach(button => button.setAttribute('value', `${button.textContent}`));
+// allButtons.forEach(button => button.addEventListener('click', display));
+
+//ADD function 
+const add = function(num1, num2) {
+	return num1+num2;
+};
+//SUBTRACT function 
+const subtract = function(num1, num2) {
+	return num1-num2;
+};
+//MULTIPLY function
+const multiply = function(num1, num2) {
+    return num1*num2;
+  };
+//DIVIDE function 
+const divide = function(num1, num2) {
+    return num1/num2;
+  };
+
+//OPERATE function 
+//takes an operator and 2 numbers and then calls one of the above functions on the numbers
+function operate(num1,operator,num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    switch (operator) {
+        case '×': 
+        return multiply(num1, num2);
+        case '+': 
+        return add(num1, num2);
+        case '−': 
+        return subtract(num1, num2);
+        case '÷': 
+        return divide(num1, num2);
+    }
+}
+
+// let initialNum = 0;
+// let num1;
+// let num2;
+// let num;
+// let string = '0';
+// let operator = '';
+// let solution = '';
+// let test = '';
+// test = string;
 
 //Add eventListener for opeartor buttons 
 //store num1 as global variable
@@ -129,36 +169,3 @@ test = string;
 // }
 
 
-//ADD function 
-const add = function(num1, num2) {
-	return num1+num2;
-};
-//SUBTRACT function 
-const subtract = function(num1, num2) {
-	return num1-num2;
-};
-//MULTIPLY function
-const multiply = function(num1, num2) {
-    return num1*num2;
-  };
-//DIVIDE function 
-const divide = function(num1, num2) {
-    return num1/num2;
-  };
-
-//OPERATE function 
-//takes an operator and 2 numbers and then calls one of the above functions on the numbers
-function operate(num1,operator,num2) {
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    switch (operator) {
-        case '×': 
-        return multiply(num1, num2);
-        case '+': 
-        return add(num1, num2);
-        case '−': 
-        return subtract(num1, num2);
-        case '÷': 
-        return divide(num1, num2);
-    }
-}
