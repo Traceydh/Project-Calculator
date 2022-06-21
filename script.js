7aa25504e53d724edddda9f186e7d1ec2d553ac5
tracey <tracey.d.huang@gmail.com>
2022-06-21 13:52:01 +1000

    Comment out old display function

--- script.js
+++ script.js
@@ -16,14 +16,8 @@ displayValue.textContent = `${initialNum}`;
 
 
 const allButtons = document.querySelectorAll('button');
-//loop through each button and set it's value as the same as it's content
 allButtons.forEach(button => button.setAttribute('value', `${button.textContent}`));
-
-//Add an eventListener for number buttons 
-
-
 allButtons.forEach(button => button.addEventListener('click', display));
-
 
 let initialNum = 0;
 let num1;
@@ -38,117 +32,101 @@ test = string;
 //Add eventListener for opeartor buttons 
 //store num1 as global variable
 
-function display() {
-    //Do not allow user type more digits than the screen can display
-    if (string.length == 10) {
-        return
-    }
-//user presses a number, store value as num1
-    //Update 0 to input number and store multiple numbers together 
-    string += this.value;
-    console.log('string: ' + string);
-    displayValue.textContent = Number(string);
-    //How does computer know when to stop storing value as num1? 
-        //When user presses an operator store num1 as is, and display operator only
-        //need to reset string of displayValue  
-        //num2 is 
-    switch (this.value) {
-        case '+':
-            console.log('string: ' + string);
-            //stored number entered as num1
-            num2 = Number(string.slice(0,-1));
-            //store operator clicked
-            operator = '+';
-            //add num1 and num2 together 
-            console.log(num1, operator, num2);
-            solution = operate(num1, operator, num2);
-            //make num2 = solution 
-            num1 = solution;
-            //reset string 
-            string = initialNum;
-            displayValue.textContent = solution;
-            console.log('solution: '+ solution);
-            break
-            //ERROR returns 0 
-        case '×':
-            num1 = Number(string.slice(0,-1));
-            operator = '×';
-            solution = operate(num1, operator, num2);
-            num2 = solution;
-            string = initialNum;
-            displayValue.textContent = solution;
-            console.log('solution: '+ solution);
-            break
-        case '−':
-            //User enters number, How do i know when the number as ended? When the human presses the operator 
-            num1 = Number(string.slice(0,-1));
-            //User enters operator, store
-            operator = '−';
-            string = '';
-            displayValue.textContent = (num1);
-                //check if there is num1 and num2 that can be applied
-            if (num2 == undefined || num1 == undefined) {
-                console.log('not defined');
-            } else {
-                num2 = string.replace('5', '');
-                console.log('num1 and num2 exist');
-            }
-            
-            
-            
-            console.log(num1, operator, num2);
-            
-            //user enters num2, How do i know when this number has ended? 
-            
+// function display() {
+//     Do not allow user type more digits than the screen can display
+//     if (string.length == 10) {
+//         return
+//     }
+// user presses a number, store value as num1
+//     Update 0 to input number and store multiple numbers together 
+//     string += this.value;
+//     console.log('string: ' + string);
+//     displayValue.textContent = Number(string);
+//     How does computer know when to stop storing value as num1? 
+//         When user presses an operator store num1 as is, and display operator only
+//         need to reset string of displayValue  
+//         num2 is 
+//     switch (this.value) {
+//         case '+':
+//             console.log('string: ' + string);
+//             stored number entered as num1
+//             num2 = Number(string.slice(0,-1));
+//             store operator clicked
+//             operator = '+';
+//             add num1 and num2 together 
+//             console.log(num1, operator, num2);
+//             solution = operate(num1, operator, num2);
+//             make num2 = solution 
+//             num1 = solution;
+//             reset string 
+//             string = initialNum;
+//             displayValue.textContent = solution;
+//             console.log('solution: '+ solution);
+//             break
+//             ERROR returns 0 
+//         case '×':
+//             num1 = Number(string.slice(0,-1));
+//             operator = '×';
+//             solution = operate(num1, operator, num2);
+//             num2 = solution;
+//             string = initialNum;
+//             displayValue.textContent = solution;
+//             console.log('solution: '+ solution);
+//             break
+//         case '−':
+//             User enters number, How do i know when the number as ended? When the human presses the operator 
+//             num1 = Number(string.slice(0,-1));
+//             User enters operator, store
+//             operator = '−';
+//             string = '';
+//             displayValue.textContent = (num1);
+//                 check if there is num1 and num2 that can be applied
+//             if (num2 == undefined || num1 == undefined) {
+//                 console.log('not defined');
+//             } else {
+//                 num2 = string.replace('5', '');
+//                 console.log('num1 and num2 exist');
+//             }
+//             break
+//         case '÷':
+//             ERROR divides by 0 
+//             num1 = Number(string.slice(0,-1));
+//             operator = '÷';
+//             solution = operate(num1, operator, num2);
+//             num2 = solution;
+//             string = initialNum;
+//             displayValue.textContent = solution;
+//             console.log('solution: '+ solution);
+//             break
+//     }
 
-            //user enters operator again
-                //check variables and execute 
-            
-            break
-        case '÷':
-            //ERROR divides by 0 
-            num1 = Number(string.slice(0,-1));
-            operator = '÷';
-            solution = operate(num1, operator, num2);
-            num2 = solution;
-            string = initialNum;
-            displayValue.textContent = solution;
-            console.log('solution: '+ solution);
-            break
-    }
+//     User enters next number, store this as num2 
+//     When user presses =, perform the operate function, display solution, reset string
+//     if (this.value == '=') {
+//         num2 = solution; 
+//         num1 = Number(string.slice(0,-1));
+//         solution = operate(num1,operator,num2);
+//         displayValue.textContent = solution;
+//         string = solution;
+//         reset num2 back to 0, otherwise it will add num2 to num1 when operator is
+//         num1 = 0;
+//     }
 
-    //User enters next number, store this as num2 
-    //When user presses =, perform the operate function, display solution, reset string
-    if (this.value == '=') {
-        num2 = solution; 
-        num1 = Number(string.slice(0,-1));
-        solution = operate(num1,operator,num2);
-        displayValue.textContent = solution;
-        string = solution;
-        //reset num2 back to 0, otherwise it will add num2 to num1 when operator is
-        num1 = 0;
-    }
+//     if AC is pressed, reset num to 0 
+//     if (this.value == 'AC') {
+//         string = 0;
+//         num1 = 0;
+//         num2 = 0;
+//         solution = 0;
+//         displayValue.textContent = `${string}`;
+//     }
 
-    //if AC is pressed, reset num to 0 
-    if (this.value == 'AC') {
-        string = 0;
-        num1 = 0;
-        num2 = 0;
-        solution = 0;
-        displayValue.textContent = `${string}`;
-    }
-
-    //Delete one digit if delete is pressed 
-    if (this.value == 'Delete') {
-        string = string.slice(0,-7);
-        displayValue.textContent = Number(string);
-    }
-}
-//ERRORS: when you press the operator, besides +, it produces incorrect answer because it computes it with num1=0 in the beginning 
-//User types num1, store 
-//user types operator, store 
-//user types num2, store 
-//user types = or operator, compute solution and display 
+//     Delete one digit if delete is pressed 
+//     if (this.value == 'Delete') {
+//         string = string.slice(0,-7);
+//         displayValue.textContent = Number(string);
+//     }
+// }
 
 
 //ADD function 
